fee1a3f09f9a1d405937ee6a897a8bbbb739227f
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-23 21:24:19 -0400

    circles and rows and columns oh my

--- sketch.js
+++ sketch.js
@@ -14,6 +14,24 @@ function setup() {
 
   background(220, 20, 20);
 
+  //drawing a row of stars
+  // for (let x = 50; x < width; x += 75) {
+    // star(x, 50, 50, 5);
+  // }
+
+  // drawing a column of stars
+  // for (let y = 50; y < width; y += 75) {
+    // star(50, y, 50, 5);
+  // }
+
+  // drawing multiple rows of rows
+  for (let y = 50; y < height; y += 60) {
+    //drawing a row of stars
+    for (let x = 50; x < width; x += 60) {
+      star(x, y, 50, 5); //the y had to be changed bc the y is changing!
+    }
+  }
+
   push();
   strokeWeight(4);
   translate(200, 200);
@@ -27,11 +45,34 @@ function setup() {
   line(50, 0, 5, -5);
   pop();
 
-  strokeWeight(4);
+  strokeWeight(2);
   star(500, 500, 50, 5);
+
+  // push();
+  // noFill();
+  // translate(500, 300);
+  // ellipse(0, 0, 40);
+  // ellipse(0, 0, 60);
+  // ellipse(0, 0, 80);
+  // pop();
+
+  push();
+  noFill();
+  strokeWeight(1);
+  translate(600, 300);
+  for (let d = 10; d < 400; d += 5) {
+    ellipse(0, 0, d);
+  }
+
+  translate(150, 50);
+  for (let d = 10; d < 400; d += 7) {
+    ellipse(0, 0, d);
+  }
+
+  for (let cnt = 0; cnt < 20; cnt++) {
+    print(cnt);
+    ellipse(0, 0, 20 * cnt);
+  }
 }
 
-function draw() {
-  strokeWeight(4);
-  star(600, 600, 50, 5);
-}
+function draw() {}
